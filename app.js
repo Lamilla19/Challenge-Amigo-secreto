f276c20826aaa2ba231aad9469f235dde57e93ba
Kevin Lamilla <Kevinlamilla16@gmail.com>
2025-01-21 21:57:44 -0500

    Funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigosSorteo = [];
+
+function agregarAmigo(){
+    let nombreParticipante = document.getElementById("amigo").value;
+    if(nombreParticipante == "" ){
+        alert("Estimado usuario , por favor ingrese un nombre de participante");
+    }else{
+        amigosSorteo.push(nombreParticipante)
+        document.getElementById("amigo").value="";
+        console.log(amigosSorteo)
+    }
+}
